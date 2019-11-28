2f20a7baa1a283adfe79e8fdfd3f2f17f1060f75
mmargauxx <margaux.laurier@gmail.com>
2019-11-28 15:24:53 +0100

    popup button color, position sticky header, padding

--- public/js/script.js
+++ public/js/script.js
@@ -5,7 +5,7 @@ L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_L
 }).addTo(cultureMap);
 function onLocationFound(e) {
     var radius = e.accuracy;
-    L.marker(e.latlng).addTo(cultureMap).bindPopup("Je bent hier " + radius + " meter in omtrek").openPopup();
+    L.marker(e.latlng).addTo(cultureMap).bindPopup("<p class='here'>START YOUR JOURNEY HERE</p><p class='here'>currently accurate to " + radius + " meters</p>").openPopup();
     L.circle(e.latlng, radius).addTo(cultureMap);
 }
 cultureMap.on('locationfound', onLocationFound);
