eeb40e02f59a1be02cc8e0dcb69a7780b8b1f194
mmargauxx <margaux.laurier@gmail.com>
2019-11-29 00:31:18 +0100

    error msg

--- public/js/script.js
+++ public/js/script.js
@@ -9,6 +9,10 @@ function onLocationFound(e) {
     L.circle(e.latlng, radius).addTo(cultureMap);
 }
 cultureMap.on('locationfound', onLocationFound);
+function onLocationError(e) {
+    alert(e.message);
+}
+cultureMap.on('locationerror', onLocationError);
 
 const basis = L.icon({
   iconUrl: '../img/marker-basis.png',
@@ -34,69 +38,3 @@ const bibliotheek = L.icon({
   iconAnchor: [0, 0],
   popupAnchor: [25, 0]
 });
-/*
-for (var i = 0; i < gebouwen.length; i++) {
-  if (gebouwen[i].length.categorie === "Museum") {
-    new L.marker(gebouwen[i].geometry, {
-      icon: museum
-    })
-  }
-}
-
-if(gebouwen[i].categorie === "Museum"){
-	new L.marker(gebouwen[i].geolocation, {
-			icon: museum
-})
-}
-
-$.getJSON(data, function(json) {
-  const gebouwen = json.gebouwen;
-  for (var i=0; i<gebouwen.length; i++) {
-
-    if (gebouwen[i].categorie === "Museum") {
-      new L.marker(gebouwen[i].geolocation, {
-          icon: museum
-        }).bindPopup(`
-					<img src="${gebouwen[i].marker}" alt="museum marker" class="popup-marker">
-					<h1>${gebouwen[i].naam}</h1>
-					<h2>${gebouwen[i].categorie}</h2>
-					<p>
-					${gebouwen[i].straat} ${gebouwen[i].huisnr} <br>
-					${gebouwen[i].postcode} ${gebouwen[i].gemeente} <br>
-					</p>
-					`).addTo(cultureMap);
-    }
-
-    if (gebouwen[i].categorie === "Archief | Erfgoedbibliotheek") {
-      new L.marker(gebouwen[i].geolocation, {
-          icon: kerk
-				}).bindPopup(`
-					<img src="${gebouwen[i].marker}" alt="kerk marker" class="popup-marker">
-					<h1>Campus ${gebouwen[i].naam}</h1>
-					<h2>${gebouwen[i].categorie}</h2>
-					<p>
-					${gebouwen[i].straat} ${gebouwen[i].huisnr} <br>
-					${gebouwen[i].postcode} ${gebouwen[i].gemeente} <br>
-					</p>
-					`)
-				.addTo(cultureMap);
-		}
-
-		if (gebouwen[i].categorie === "Archief | Erfgoedbibliotheek") {
-			new L.marker(gebouwen[i].geolocation, {
-					icon: bibliotheek
-				}).bindPopup(`
-					<img src="${gebouwen[i].marker}" alt="bibliotheek marker" class="popup-marker">
-					<h1>Campus ${gebouwen[i].naam}</h1>
-					<h2>${gebouwen[i].categorie}</h2>
-					<p>
-					${gebouwen[i].straat} ${gebouwen[i].huisnr} <br>
-					${gebouwen[i].postcode} ${gebouwen[i].gemeente} <br>
-					</p>
-					`)
-				.addTo(cultureMap);
-		}
-				//function onClick(e) {
-      	//window.open(this.options.win_url); }
-  }
-});*/
